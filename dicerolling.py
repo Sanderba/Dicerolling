15497d64f719e22af9da654af9b7af63d3186a09
Sanderba <108481205+Sanderba@users.noreply.github.com>
2022-10-27 21:15:22 +0200

    Update dicerolling.py
    Added colours and draw

--- dicerolling.py
+++ dicerolling.py
@@ -1,30 +1,35 @@
 import math
 import random
 import time
+import colorama
+
 
 dice = random.randint(1,6)
 computerDice = random.randint(1,6)
 
 
-def rolldice(roll):
-    print("Dice 1 is rolling...")
+def rolldice(roll, color=colorama.Fore.YELLOW):
+    print(color+"You are rolling...")
     time.sleep(1)
     print("You rolled: ", + dice)
     print("-------------------")
     
-def computerRoll(computerRoll):
+def computerRoll(computerRoll, color=colorama.Fore.CYAN):
+    print(color +"Computer rolled: ", +computerDice)
     print("Computer Dice is rolling...")
     time.sleep(1)
-    print("Computer rolled: ", +computerDice)
     
-    
-        
+
 rolldice(dice)
 computerRoll(computerRoll)
+print(colorama.Fore.RESET)        
 print("\n")
 
 if dice > computerDice:
-    print("You won!")
+    print(colorama.Fore.GREEN + "You won!")
 elif computerDice > dice:
-    print("Computer Won!")
-print("\n")
+    print(colorama.Fore.RED + "Computer Won!")
+elif dice == computerDice:
+    print("Draw!")    
+
+print(colorama.Fore.RESET)   
